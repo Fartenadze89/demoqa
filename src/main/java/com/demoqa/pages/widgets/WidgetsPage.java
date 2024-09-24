3fb84bfe476b4c2e2455b65121a6c5b0b5b45e52
Fartenadze89 <nfartenadze89@gmail.com>
2024-09-24 14:52:22 +0400

    fix: date picker locator

--- src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
+++ src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
@@ -9,7 +9,7 @@ public class WidgetsPage extends HomePage {
     private By selectMenuItem = By.xpath("  //li[@id='item-8']/span[text()='Select Menu']");
 
 
-    private By datePickerMenuItem = By.xpath("  //li[@id='item-8']/span[text()='Date Picker']");
+    private By datePickerMenuItem = By.xpath("  //li[@id='item-2']/span[text()='Date Picker']");
 
     public SelectMenuPage clickSelectMenu() {
         scrollToElementJS(selectMenuItem);
